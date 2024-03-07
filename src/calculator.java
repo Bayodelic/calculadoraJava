822841158ca38e5e79a5f8ed4558d243d8daab9d
Braulio Gonzalez <braulio27033@gmail.com>
2024-03-06 22:08:16 -0600

    User inputs number to the calculator

--- src/calculator.java
+++ src/calculator.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
+
 public class calculator {
     public static void main(String[] args){
         System.out.println("Calculator in Java");
         Mathematics calculator = new Mathematics();
 
-        System.out.println(calculator.add(4,4));
-        System.out.println(calculator.subtraction(8,3));
-        System.out.println(calculator.multiplication(4,4));
-        System.out.println(calculator.division(16,4));
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Input the first number");
+        double num1 = read.nextDouble();
+        System.out.println("Input the second number");
+        double num2 = read.nextDouble();
+
+        System.out.println(calculator.add(num1,num2));
+        System.out.println(calculator.subtraction(num1,num2));
+        System.out.println(calculator.multiplication(num1,num2));
+        System.out.println(calculator.division(num1,num2));
 
 
     }
